8b2f62b0e9365955aa41a55773f68c5e9c8a8d14
DanielTzafrir <74878286+DanielTzafrir@users.noreply.github.com>
2020-11-26 21:18:02 +0200

    Add files via upload

--- ex1/tests/WGraph_AlgoTest.java
+++ ex1/tests/WGraph_AlgoTest.java
@@ -11,62 +11,48 @@ public class WGraph_AlgoTest {
     public void test() throws FileNotFoundException {
             long start = System.currentTimeMillis();
 
-            weighted_graph g = new WGraph_DS();
-            g.addNode(0);
-            g.addNode(1);
-            g.addNode(2);
-            g.addNode(3);
-            g.addNode(4);
-            g.connect(0,4, 29);
-            g.connect(0,1, 41);
-            g.connect(0,2, 17);
-            g.connect(4,1, 9);
-            g.connect(4,2, 9);
-            g.connect(3,2, 11);
+            weighted_graph weightedGraph = new WGraph_DS();
+            weightedGraph.addNode(0);
+            weightedGraph.addNode(1);
+            weightedGraph.addNode(2);
+            weightedGraph.connect(0,1, 6);
+            weightedGraph.connect(0,2, 9);
+            weightedGraph.connect(3,2, 13);
 
-            Test2(g);
+            Test2(weightedGraph);
+            Test3(weightedGraph);
 
-            System.out.println("Test 2 Runtime: " + (System.currentTimeMillis() - start)/1000F + "\n");
-
-            start = System.currentTimeMillis();
-
-            Test3(g);
-
-            System.out.println("Test 3 Runtime: " + (System.currentTimeMillis() - start)/1000F + "\n");
         }
 
-        // Shortest path + distance test
-        public static void Test2(weighted_graph graph) {
-            weighted_graph_algorithms wGraphAlgo = new WGraph_Algo(graph);
+        public static void Test2(weighted_graph weighted_graph) {
+            weighted_graph_algorithms graph_algo = new WGraph_Algo(weighted_graph);
 
-            for(int i = 0; i < graph.nodeSize(); i++) {
-                for(int j = 0; j < graph.nodeSize(); j++) {
-                    System.out.println("Shortest path from " + i + " to " + j + " is: " + wGraphAlgo.shortestPathDist(i, j));
-                    List<node_info> path = wGraphAlgo.shortestPath(i,j);
-                    System.out.println("Path: " + ((path == null) ? "None" : path.toString()) + "\n");
+            for(int i = 0; i < weighted_graph.nodeSize(); i++) {
+                for(int j = 0; j < weighted_graph.nodeSize(); j++) {
+                    System.out.println(" The Shortest path: " + i + " ==> " + j + ": " + graph_algo.shortestPathDist(i, j));
+                    List<node_info> path = graph_algo.shortestPath(i,j);
+                    System.out.println(path.toString());
                 }
             }
         }
 
-        // Save, Load, Init and copy test
+
         public static void Test3(weighted_graph graph) throws FileNotFoundException {
             weighted_graph_algorithms wGraphAlgo = new WGraph_Algo(graph);
-            weighted_graph initGraph;
-            weighted_graph copyGraph;
+            weighted_graph init;
+            weighted_graph copy;
 
-            wGraphAlgo.save("graph1.txt");
-            wGraphAlgo.load("mistake.txt"); // file not found exception
-            wGraphAlgo.load("graph1.txt");
+            wGraphAlgo.save("graph.txt");
+            wGraphAlgo.load("graph.txt");
 
-            initGraph = wGraphAlgo.getGraph();
+            init = wGraphAlgo.getGraph();
+            copy = wGraphAlgo.copy();
+            wGraphAlgo.init(init);
 
-            wGraphAlgo.init(initGraph);
-
-            copyGraph = wGraphAlgo.copy();
 
             System.out.println(graph);
-            System.out.println(initGraph);
-            System.out.println(copyGraph);
+            System.out.println(init);
+            System.out.println(copy);
         }
     }
 
--- ex1/tests/WGraph_DSTest.java
+++ ex1/tests/WGraph_DSTest.java
@@ -7,36 +7,33 @@ import org.junit.jupiter.api.Test;
 public class WGraph_DSTest {
     @Test
     public static void main(String[] args) {
-        long start = System.currentTimeMillis();
         Test1();
-        System.out.println("Test 1 Runtime: " + (System.currentTimeMillis() - start)/1000F + "\n");
     }
 
-    public static weighted_graph createGraph(int node_range, int edge_range) {
-        weighted_graph g = new WGraph_DS();
-
+    public static weighted_graph createGraph(int nodeRange, int edgeRange) {
+        weighted_graph wGraph_ds = new WGraph_DS();
         int i = 0;
 
-        while(i < node_range) {
-            g.addNode(i++);
+        while(i < nodeRange) {
+            wGraph_ds.addNode(i++);
         }
 
-        while(g.edgeSize() < edge_range) {
-            int a = (int)(Math.random() * node_range);
-            int b = (int)(Math.random() * node_range);
-            double w = Math.max(50,Math.random() * 100);
+        while(wGraph_ds.edgeSize() < edgeRange) {
+            int randomNode1 = (int)(Math.random() * nodeRange);
+            int randomNode2 = (int)(Math.random() * nodeRange);
+            double w = Math.max(20,Math.random() * 50);
 
-            g.connect(a, b, w);
+            wGraph_ds.connect(randomNode1, randomNode2, w);
         }
 
-        return g;
+        return wGraph_ds;
     }
 
     // create a graph with 1,000,000 nodes and 2,000,000 edges
     public static void Test1() {
 
             weighted_graph g = createGraph(1000000, 2000000);
-            System.out.println("Successfully created graph with " + 1000000 + " nodes and " + 2000000 + " edges!");
+            System.out.println("Successfully created the graph in test 1!");
 
     }
 }
